f1ac4cccd54faaa3dc3ba6ab5e0ef37407d296ce
qwu1204 <jolesia.wu@gmail.com>
2016-04-13 18:19:58 -0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -2,12 +2,40 @@
 var correct = 0;
 
 // prompt
-var answer1 = prompt("ask a question...");
-console.log(answer1);
+var answer1 = prompt("What is the name of the latest iPhone");
+
+//console.log(answer1);
 // if statement
-if ( answer1.toUpperCase() === 'MAGIC' ) {
+if ( answer1.toUpperCase() === 'SE' ) {
  correct += 1;
 }
-console.log("correct : "+ correct);
+
+var answer2 = prompt("What is the name of the latest iPhone");
+
+//console.log(answer1);
+// if statement
+if ( answer2.toUpperCase() === 'SE' ) {
+ correct += 1;
+}
+
+var answer3 = prompt("What is this script written in");
+
+//console.log(answer1);
+// if statement
+if ( answer3.toUpperCase() === 'JAVASCRIPT' ) {
+ correct += 1;
+}
+
+var answer3 = prompt("Is Javascript and Java related at all? Answer yes or no");
+
+//console.log(answer1);
+// if statement
+if ( answer3.toUpperCase() === 'no' ) {
+ correct += 1;
+}
+
+
+
+//console.log("correct : "+ correct);
 // output results
-document.write("<p>write something to page...</p>");
+document.write("<p>You got " + correct + " out of 5 right.</p>");
